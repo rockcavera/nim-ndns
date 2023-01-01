e1ae5fc6b0116e5b82d293005410fb0c8f80f39e
rockcavera <rockcavera@gmail.com>
2023-01-01 03:39:10 -0300

    Fix OpenBSD (#2)
    * try fix openbsd resolv
    
    * fix typo
    
    * trying again
    
    * again
    
    * again
    
    * again
    
    * again
    
    * fix ready

--- src/ndns/platforms/resolv.nim
+++ src/ndns/platforms/resolv.nim
@@ -10,7 +10,9 @@
 ## Using this module implies passing `-lresolv` or `-lc` to the linkage process.
 ##
 ## To use the interface deprecated by the resolv library, compile with
-## `-d:useDeprecatedResolv`.
+## `-d:useDeprecatedResolv`. On OpenBSD it is recommended to define this symbol
+## when compiling, since the `resolv.h` used on this platform has its own
+## definitions that will only be used when this symbol is defined.
 
 when defined(nimdoc):
   import std/[net, winlean]
@@ -23,9 +25,31 @@ else:
   {.passL: "-lresolv".}
 
 const
+  useOpenBSDResolv = when defined(useDeprecatedResolv) and defined(openbsd): true
+                     else: false
+    # The structure of res_state in OpenBSD has several peculiarities, as well
+    # as currently adopts the deprecated version with res_init().
+    # https://github.com/openbsd/src/blob/e3c5fa921ef394179421471c88eb2be26d8a6692/include/resolv.h
+
   MAXNS = 3
   MAXDNSRCH = 6
   MAXRESOLVSORT = 10
+
+when useOpenBSDResolv:
+  {.emit: """/*INCLUDESECTION*/
+#include <netinet/in.h>
+""".} # See https://github.com/troglobit/inadyn/issues/241
+
+  const MAXDNSLUS = 4
+
+  type
+    CulongOrCuint = cuint
+
+    ResTimeSpecObj = object
+      resSec: Time
+      resNSec: clong
+else:
+  type CulongOrCuint = culong
 
 type
   ResSendhookact {.size: 4.} = enum
@@ -40,52 +64,65 @@ type
 
   SortListObj = object
     `addr`: InAddr
-    mask: cuint
+    mask: uint32 # uint32_t
 
   Ext = object
-    nscount: cushort
-    nsmap: array[MAXNS, cushort]
+    nscount: uint16 # u_int16_t
+    nsmap: array[MAXNS, uint16]
     nssocks: array[MAXNS, cint]
-    nscount6: cushort
-    nsinit: cushort
+    nscount6: uint16
+    nsinit: uint16
     nsaddrs: array[MAXNS, ptr Sockaddr_in6]
-    initstamp: culonglong
+    initstamp: array[2, cuint]
 
   UUnion {.union.} = object
-    pad: array[52, char]
+    pad: array[52, cchar]
     ext: Ext
 
   ResState = object
     retrans: cint
     retry: cint
-    options: culong
+    options: CulongOrCuint
     nscount: cint
+    when useOpenBSDResolv:
+      family: array[2, cint]
     nsaddrList: array[MAXNS, Sockaddr_in]
     id: cushort
     dnsrch: array[MAXDNSRCH + 1, cstring]
-    defdname: array[256, char]
-    pfcode: culong
+    defdname: array[256, cchar]
+    pfcode: CulongOrCuint
     ndots {.bitsize:4.}: cuint
     nsort {.bitsize:4.}: cuint
-    ipv6_unavail {.bitsize:1.}: cuint
-    unused {.bitsize:23.}: cuint
+    when useOpenBSDResolv:
+      unused: array[3, cchar]
+    else:
+      ipv6_unavail {.bitsize:1.}: cuint
+      unused {.bitsize:23.}: cuint
     sortList: array[MAXRESOLVSORT, SortListObj]
-    qhook: ResSendQhook
-    rhook: ResSendRhook
-    resHRrrno: cint
-    vcsock: cint
-    flags: cuint
-    u: UUnion
+    when useOpenBSDResolv:
+      lookups: array[MAXDNSLUS, cchar]
+      restimespec: ResTimeSpecObj
+      reschktime: Time
+    else:
+      qhook: ResSendQhook
+      rhook: ResSendRhook
+      resHErrno: cint
+      vcsock: cint
+      flags: cuint
+      u: UUnion
 
-proc resNinit(statep: var ResState): cint {.importc: "res_ninit", header: "<resolv.h>".}
-proc resNclose(rstatep: var ResState) {.importc: "res_nclose", header: "<resolv.h>".}
+when not defined(useDeprecatedResolv):
+  proc resNInit(statep: var ResState): cint {.importc: "res_ninit", header: "<resolv.h>".}
+  proc resNClose(rstatep: var ResState) {.importc: "res_nclose", header: "<resolv.h>".}
+else:
+  type SResState {.importc: "struct __res_state".} = object
 
-when defined(useDeprecatedResolv):
-  type
-    SResState {.importc: "struct __res_state".} = object
+  when useOpenBSDResolv:
+    var res {.importc: "_res", nodecl.}: SResState
+  else:
+    proc resState(): ptr SResState {.importc: "__res_state".}
 
   proc resInit(): cint {.importc: "res_init", header: "<resolv.h>".}
-  proc resState(): ptr SResState {.importc: "__res_state".}
 
 proc getSystemDnsServer*(): string =
   ## Returns the IPv4 used by the system for DNS resolution. Otherwise it
@@ -96,14 +133,19 @@ proc getSystemDnsServer*(): string =
 
   when defined(useDeprecatedResolv):
     if resInit() == 0:
-      fromSockAddr(cast[ResState](resState()[]).nsaddrList[0], sizeof(Sockaddr_in).SockLen, ip, port)
+      when useOpenBSDResolv:
+        let saddr = cast[ResState](res).nsaddrList[0]
+      else:
+        let saddr = cast[ResState](resState()[]).nsaddrList[0]
+
+      fromSockAddr(saddr, sizeof(Sockaddr_in).SockLen, ip, port)
 
       result = $ip
   else:
     var rs: ResState
 
-    if resNinit(rs) == 0:
+    if resNInit(rs) == 0:
       fromSockAddr(rs.nsaddrList[0], sizeof(Sockaddr_in).SockLen, ip, port)
-      resNclose(rs)
+      resNClose(rs)
 
       result = $ip
